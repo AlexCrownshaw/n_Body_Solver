256b9ec0013d072a150c2b9edcce14f1de550402
alexc <alex.crownshaw12@gmail.com>
2023-09-10 16:45:02 +0100

    Installable package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='n_body_solver',
+    version='0.1.0',
+    description='A DNS n body solver using newtons law of gravity',
+    url='https://github.com/AlexCrownshaw/n_Body_Solver',
+    author='Alex Crownshaw',
+    author_email='alex.crownshaw12@gmail.com',
+    packages=['n_body_solver'],
+    install_requires=["matplotlib==3.5.1", "numpy==1.23.4", "pandas==1.5.1"]
+)
